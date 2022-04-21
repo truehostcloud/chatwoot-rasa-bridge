2cb0388f56d9167c947469db9d3cfd0c7bda5a54
William Mwai <willymwaid92@gmail.com>
2022-04-21 13:35:20 +0300

    chore: configurable rasa token

--- app.py
+++ app.py
@@ -5,13 +5,19 @@ from flask import Flask, request
 rasa_url = os.environ.get("RASA_URL")
 chatwoot_url = os.environ.get("CHATWOOT_URL")
 chatwoot_bot_token = os.environ.get("CHATWOOT_BOT_TOKEN")
+rasa_channel = os.environ.get("RASA_CHANNEL")
+rasa_jwt_token = os.environ.get("RASA_JWT_TOKEN")
 
 
 def send_to_bot(sender, message):
     data = {"sender": sender, "message": message}
     headers = {"Content-Type": "application/json", "Accept": "application/json"}
 
-    r = requests.post(f"{rasa_url}/webhooks/rest/webhook", json=data, headers=headers)
+    r = requests.post(
+        f"{rasa_url}/webhooks/{rasa_channel}/webhook?token={rasa_jwt_token}",
+        json=data,
+        headers=headers,
+    )
     return r.json()[0]["text"]
 
 
@@ -31,7 +37,7 @@ def send_to_chatwoot(account, conversation, message):
 app = Flask(__name__)
 
 
-@app.route("/rasa", methods=["POST"])
+@app.route("/", methods=["POST"])
 def rasa():
     data = request.get_json()
     message_type = data["message_type"]
