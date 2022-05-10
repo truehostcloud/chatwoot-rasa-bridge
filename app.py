38b9e58db98f0da160d2bad1577878c547edc2db
William Mwai <willymwaid92@gmail.com>
2022-05-10 11:09:57 +0300

    chore: append conversation id to rasa username, skip open conversations

--- app.py
+++ app.py
@@ -22,22 +22,23 @@ def extract_bot_response(response_json):
             if response_object.get("buttons"):
                 buttons_object = response_object.get("buttons")
                 for button in buttons_object:
-                    response_button_list.append({
-                        "title": button.get("title"),
-                        "value": button.get("payload"),
-                    })
+                    response_button_list.append(
+                        {
+                            "title": button.get("title"),
+                            "value": button.get("payload"),
+                        }
+                    )
         response_text = "\n".join(response_text_list)
     else:
         response_text = response_json.get("message")
     return response_text, response_button_list
 
 
-def send_to_bot(sender, message):
-    data = {"sender": sender, "message": message}
-    jwt_payload = {"user": {"username": sender, "role": "guest"}}
-    rasa_jwt_token = jwt.encode(
-        jwt_payload, rasa_jwt_token_secret, algorithm="HS256"
-    )
+def send_to_bot(sender, message, conversation_id):
+    username = f"{sender}_{conversation_id}"
+    data = {"sender": username, "message": message}
+    jwt_payload = {"user": {"username": username, "role": "guest"}}
+    rasa_jwt_token = jwt.encode(jwt_payload, rasa_jwt_token_secret, algorithm="HS256")
     headers = {
         "Content-Type": "application/json",
         "Accept": "application/json",
@@ -78,22 +79,30 @@ app = Flask(__name__)
 @app.route("/", methods=["POST"])
 def rasa():
     data = request.get_json()
-    message_type = data["message_type"]
-    message = data["content"]
-    conversation = data["conversation"]["id"]
-    contact = data["sender"]["id"]
-    account = data["account"]["id"]
+    message_type = data.get("message_type")
+    message = data.get("content")
+    conversation = data.get("conversation", {})
+    conversation_id = conversation.get("id")
+    contact = data.get("sender", {}).get("id")
+    account = data.get("account").get("id")
     create_message = {}
+    conversation_status = data.get("conversation", {}).get("status")
     if data.get("event") == "message_updated":
         contact = data["conversation"]["contact_inbox"]["contact_id"]
         content_attributes = data["content_attributes"]
         submitted_values = content_attributes.get("submitted_values", [])
-        submitted_values_text_list = [submitted_text.get("value") for submitted_text in submitted_values]
+        submitted_values_text_list = [
+            submitted_text.get("value") for submitted_text in submitted_values
+        ]
         message = "\n".join(submitted_values_text_list)
 
-    if message_type == "incoming" or data.get("event") == "message_updated":
-        text_response, response_button_list = send_to_bot(contact, message)
-        create_message = send_to_chatwoot(account, conversation, text_response, response_button_list)
+    if (
+        message_type == "incoming" or data.get("event") == "message_updated"
+    ) and conversation_status == "pending":
+        text_response, response_button_list = send_to_bot(contact, message, conversation_id)
+        create_message = send_to_chatwoot(
+            account, conversation_id, text_response, response_button_list
+        )
     return create_message
 
 
